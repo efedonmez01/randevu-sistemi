fcfcbf837408aa026fe4cd8e4720cf6ecbfb7a95
efedonmez01 <efeyagizdonmez1@yahoo.com>
2025-06-18 07:16:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -90,6 +90,8 @@ app.get('/randevular', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
     console.log(`Sunucu çalışıyor: http://localhost:${PORT}`);
 });
+
